f4e68c29e657315d379dd58b75f1b63d7ec35331
Evilbonesss <stepanpopov2005@gmail.com>
2025-06-13 15:13:42 +0300

    final

--- Monsters-main/Monsters-main/Monsters/src/main/java/model/importers/FileImporter.java
+++ Monsters-main/Monsters-main/Monsters/src/main/java/model/importers/FileImporter.java
@@ -4,10 +4,6 @@ import java.io.File;
 import java.util.List;
 import model.Monster;
 
-/**
- *
- * @author tsyga
- */
 public interface FileImporter {
     void setNext(FileImporter next);
     List<Monster> importFile(File file) throws Exception;
